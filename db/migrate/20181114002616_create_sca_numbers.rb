0add54c86de299d353341c749d5b7d1f1a0c38e6
yutoushio <yutoushio@Shigeto-no-MacBook-Air.local>
2019-02-08 03:33:52 +0900

    update

--- db/migrate/20181114002616_create_sca_numbers.rb
+++ db/migrate/20181114002616_create_sca_numbers.rb
@@ -2,7 +2,7 @@ class CreateScaNumbers < ActiveRecord::Migration[5.2]
   def change
     create_table :sca_numbers do |t|
       t.float :number
-      t.references :Scatter, foreign_key: true
+      t.references :scatter, foreign_key: true
 
       t.timestamps
     end
